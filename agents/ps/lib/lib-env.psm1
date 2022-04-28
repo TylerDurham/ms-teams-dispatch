fa920208082a8d6e882fa643a94343b182a5c0ae
Tyler Durham <TylerDurham@users.noreply.github.com>
2022-04-28 12:35:59 -0500

    Error handling for settings file.

--- agents/ps/lib/lib-env.psm1
+++ agents/ps/lib/lib-env.psm1
@@ -1,15 +1,28 @@
 Function Load-Env {
     param($name = "production")
 
-    $settingsFile = (Split-Path -Path $PSScriptRoot -Parent) + "\local.settings.json" # (Resolve-Path -Path "local.settings.json")
+    try {
 
-    $config = (Get-Content -Path $settingsFile | ConvertFrom-Json -AsHashtable).env
-    $env = $config[$name]
-    
-    $env.Keys | ForEach-Object {
-        #Write-Host $_
-        #Write-Host $env[$_]
-        Set-Item "ENV:$_" $env[$_]
+
+        $settingsFile = (Split-Path -Path $PSScriptRoot -Parent) + "\local.settings.json" # (Resolve-Path -Path "local.settings.json")
+
+        if ( -Not (Test-Path -Path $settingsFile )) {
+            Write-Error "Could not locate file $settingsFile. Aborting."
+            Exit
+        }
+
+
+        $config = (Get-Content -Path $settingsFile | ConvertFrom-Json -AsHashtable).env
+        $env = $config[$name]
+        
+        $env.Keys | ForEach-Object {
+            #Write-Host $_
+            #Write-Host $env[$_]
+            Set-Item "ENV:$_" $env[$_]
+        }
+    } catch {
+        Write-Error "Could not load configuration file! "
+        Exit
     }
 }
 
